4d8bd6b40f0da61557d9b26e802eed3d74462bbc
Md. Ashraful Islam <mdashrafulislam.inbox@gmail.com>
2026-01-13 19:54:27 +1100

    declaring seasons part

--- Solution_Q2.py
+++ Solution_Q2.py
@@ -1,2 +1,12 @@
 # Solution Q2
 
+import os
+import csv
+import math
+# Here we have declared the months according to the questions
+SEASONS = {
+    "Summer": ["December", "January", "February"],
+    "Autumn": ["March", "April", "May"],
+    "Winter": ["June", "July", "August"],
+    "Spring": ["September", "October", "November"]
+}
